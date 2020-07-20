e697029d80785a06d0f73e867b439a0dc208497e
Laubersheini <d.laubersheimer@t-online.de>
2020-07-20 16:37:29 +0200

    3 different color modes

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 var AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
 context = new AudioContext();
-var barCount = 256 //must be a power of 2 ?
+var barCount = 256 //must be a power of 2 //2048 is the most my laptop can handle
 const fftsize =  barCount*2;
-const rightBarCutoff = 30; //fft gets frequencies higher than 20kHz that are not hearable/ relevant for music
+const rightBarCutoff = 0; //fft gets frequencies higher than 20kHz that are not hearable/ relevant for music
  barCount -= rightBarCutoff
 const green = "#00ff00";
 const yellow = "#ffff00";
 const red = "#ff0000"
 const colors = [green,yellow,red];
 const colorTresholdes = [170,200,255]//must be sorted
+const backgroundColor = "#000000"
 
-
+var currentColor = "gyr";
+var rainbowColors = [];
+var barColor = "#ffffff";
 var canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
 
@@ -47,7 +50,9 @@ function render() {
   analyser.getByteFrequencyData(freq);
 //console.log(freq)
   // Iterate over the frequencies.
-  ctx.clearRect(0,0,canvas.width,canvas.height)
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0,0,canvas.width,canvas.height)
+
   for (var i = 0; i < freq.length; i++) {
     var value;
 
@@ -55,16 +60,8 @@ function render() {
 //console.log(value);
   //  ctx.fillStyle = "#000000";
     //ctx.fillStyle = "#" + value.toString(16) +""+ (255-value).toString(16)+"00"; for weird colors that slowly blend towards red
-/*    if(value<100){ //when the treshhold is reaced the entire bar changes colors
-      ctx.fillStyle = green;
 
-    }else if(value<200){
-        ctx.fillStyle = yellow;
-    }else{
-        ctx.fillStyle = red;
-    }
-*/
-
+  if(currentColor == "gyr"){
   for(let j=colorTresholdes.length-1;j>=0;j--){
       ctx.fillStyle = colors[j]
       if(value<colorTresholdes[j]){
@@ -78,11 +75,18 @@ function render() {
 
   }
 
-    var percent = i / freq.length;
-    var x = Math.round(percent * this.width);
+}else if(currentColor== "singleColor"){
+  ctx.fillStyle = barColor;
 
-    // draw the line at the right side of the canvas
+  ctx.fillRect(i/barCount*canvas.width ,canvas.height-value/255*canvas.height,
+    canvas.width/barCount   ,  canvas.height);
 
+}else if (currentColor == "rainbow") {
+  ctx.fillStyle = rainbowColors[i];
+
+  ctx.fillRect(i/barCount*canvas.width ,canvas.height-value/255*canvas.height,
+    canvas.width/barCount   ,  canvas.height);
+}
   }
 
 requestAnimationFrame(render)
@@ -94,14 +98,33 @@ console.warn("onStreamError")
 
 //callbacks
 
-function updateGain(){
-//console.log(document.getElementById("gainSlider").value)
+function generateRainbow(){
+  var r = 255;
+  var g = 0;
+  var b = 0;
+  var stepSize = 255/barCount*4;
+  for(let i=0; i<barCount/4;i++){
+    g += stepSize;
+    rainbowColors[i] = "#"+ Math.round(r).toString(16).padStart(2,"0")+Math.round(g).toString(16).padStart(2,"0")+Math.round(b).toString(16).padStart(2,"0");
+  }
+  //yellow
 
-gain.gain.value = document.getElementById("gainSlider").value
-
-
+  for(let i=barCount/4; i<barCount/2;i++){
+    r -= stepSize;
+  rainbowColors[i] = "#"+ Math.round(r).toString(16).padStart(2,"0")+Math.round(g).toString(16).padStart(2,"0")+Math.round(b).toString(16).padStart(2,"0");
+  }
+//green
+for(let i=barCount/2; i<barCount/4 *3;i++){
+  b += stepSize;
+rainbowColors[i] = "#"+ Math.round(r).toString(16).padStart(2,"0")+Math.round(g).toString(16).padStart(2,"0")+Math.round(b).toString(16).padStart(2,"0");
 }
-
+//cyan
+for(let i=barCount/4 *3; i<barCount;i++){
+  g -= stepSize;
+  rainbowColors[i] = "#"+ Math.round(r).toString(16).padStart(2,"0")+Math.round(g).toString(16).padStart(2,"0")+Math.round(b).toString(16).padStart(2,"0");
+}
+}
+generateRainbow();
 window.addEventListener('resize', resize);
 function resize(){
 canvas.width = window.innerWidth;
---
+++ settings.js
@@ -0,0 +1,57 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  console.log("hi")
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function updateGain(){
+//console.log(document.getElementById("gainSlider").value)
+
+gain.gain.value = document.getElementById("gainSlider").value
+
+
+}
+
+
+
+function updateColoring(){
+
+  var radios = document.getElementsByName('coloring');
+  var radioValue;
+for (var i = 0, length = radios.length; i < length; i++) {
+  if (radios[i].checked) {
+    // do whatever you want with the checked radio
+    radioValue= radios[i].value;
+
+    // only one radio can be logically checked, don't check the rest
+    break;
+  }
+}
+barColor = document.getElementById("singleColor").value;
+currentColor = radioValue;
+}
+function updateSingleColor(){
+barColor = document.getElementById("singleColor").value;
+
+}
